66a3882a8019c33ba8592e62044f643081cf5f16
yz9 <yzdslloli@163.com>
2018-04-27 18:53:01 +0800

    news

--- IFE2018/js/index.js
+++ IFE2018/js/index.js
@@ -1,1 +1,2 @@
 //import san01 from './san-01.js';
+import san02 from './san-02.js';
--- IFE2018/js/san-02.js
+++ IFE2018/js/san-02.js
@@ -1,18 +1,39 @@
 var MyApp = san.defineComponent({
     template:`<div>
-        
+        <input type="text" placeholder="请输入姓名" value="{=user.name=}" required="required"> 
+        <button on-click="addInfo">添加</button>
+        <table style="border:1px solid #ddd">
+            <thead>
+                <tr>
+                    <td>姓名</td><td>审核状态</td><td>操作</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr s-for="u in users">
+                    <td>u.name</td>
+                    <td>u.type</td>
+                    <td><button s-if="isCheck" on-click="check">审核</button>
+                        <button s-else on-click="delInfo">删除</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>`,
     initDate:function(){
         return{
+            arr:[1,2],
             user:{
-                name:'',
-                age:null,
-                info:'',
+                name:"",
+                type:"待审核"
             }
         }
     },
-    removeInfo:function(){
-        this.data.set("user",{name:'',age:null,info:'',})
+    addInfo:function(){
+        let u=this.data.get("user");
+        console.log(this.data.get("arr"));
+        this.data.set("arr","22");
+        //this.data.set("users","aa");
+        console.log(this.data.get("arr"));
     }
 });
 var myApp = new MyApp();
