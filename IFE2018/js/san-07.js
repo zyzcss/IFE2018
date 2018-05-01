8e1f21f097c1e4c2c0a8554887dc2e1056119c4f
zyzcss <34860281+zyzcss@users.noreply.github.com>
2018-05-01 11:34:22 +0800

    add san-07 note

--- IFE2018/js/san-07.js
+++ IFE2018/js/san-07.js
@@ -15,20 +15,23 @@ let app=san.defineComponent({
 	},
 	hook:{
 		enter: function (el, done) {
+			//不能生效 为什么done函数为空？
 			el.classList.remove("leave");
 			el.classList.add("before-enter");
 			el.classList.add("enter");
 			el.classList.remove("before-enter");
-        },
-
-        leave: function (el, done) {
-        	el.classList.remove("enter");
+			
+        	},
+		
+        	leave: function (el, done) {
+			el.classList.remove("enter");
 			el.classList.add("before-leave");
 			el.classList.add("leave");
 			el.classList.remove("before-leave");
-        	setTimeout(done,500);
-        	
-        }    
+			//使用延时器会有BUG
+			//el.style.opacity获取为空 不能用if判断
+        		setTimeout(done,500);
+        	}    
 	}
 })
 let myApp=new app();
